15cad7e22cda4d6ade3ce0515c366f90fbadbc0c
Alan Nato <alanwnato@gmail.com>
2024-11-14 16:40:59 -0600

    fix : small improvements

--- C/grades/grades.c
+++ C/grades/grades.c
@@ -4,18 +4,30 @@
 * Calculate Sum,Average
 * Display Total
 */
-int marks,total,average;
+int marks,total,average,n,i;
+
 int main(void)
 {
 
-
+	printf("How many subjects?");
+	scanf("%d",&n);
+	
+	for (i=0; i < n; i++) {
 	printf("Please input the marks:\n");
 	scanf("%d",&marks);
+	
+	while (marks <0)
+	{
+	printf("Please input the marks:\n");
+	scanf("%d",&marks);
+	}
 	total = marks + total;	
-	average = marks/3;
+	}
+	
+	average = marks/n;
 
 	
 	printf("%d\n",total);
-	printf("%d\n",marks);
+	printf("%d\n",average);
 	return 0;
 }
